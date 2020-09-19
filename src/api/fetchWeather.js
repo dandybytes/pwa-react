91b1326ad55d516d1b0a99ea6ef2f5b322ceb464
dandybytes <dandybytes@gmx.com>
2020-09-19 22:17:24 +0200

    Create basic weather fetching app

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f33a484cf794d08d0148764789aaba32';
+
+export const fetchWeather = async query => {
+    const {data} = await axios.get(url, {params: {q: query, units: 'metric', APPID: apiKey}});
+    return data;
+};
